be359ea4af1e5c736da90f425c99456148c869d0
阿棋 <2932234796@qq.com>
2022-01-26 03:02:48 +0800

    修复了lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,12 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** DO it and when n get even it returns n/2, when n get odd, it returns 3n + 1 */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if(n % 2 == 0){
+            return n/2;
         }
+        return 3 * n + 1;
     }
 
     public static void main(String[] args) {
